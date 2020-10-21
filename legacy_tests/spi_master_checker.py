22ed9830fa9a737f86892a7d41c5deddf05f853a
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2020-10-21 15:41:54 -0400

    Fix spacing in Python for readability

--- legacy_tests/spi_master_checker.py
+++ legacy_tests/spi_master_checker.py
@@ -25,15 +25,15 @@ class SPIMasterChecker(xmostest.SimThread):
         sck_value = xsi.sample_port_pins(self._sck_port)
         ss_value = []
 
-	for i in range(len(self._ss_ports)):
-          ss_value.append(xsi.sample_port_pins(self._ss_ports[i]))
+        for i in range(len(self._ss_ports)):
+            ss_value.append(xsi.sample_port_pins(self._ss_ports[i]))
 
         print "SPI Master checker started"
         while True:
             #first do the setup rx
             strobe_val = xsi.sample_port_pins(self._setup_strobe_port)
-	    if strobe_val == 1:
-              self.wait_for_port_pins_change([self._setup_strobe_port])
+            if strobe_val == 1:
+                self.wait_for_port_pins_change([self._setup_strobe_port])
 
             expected_cpol = self.get_setup_data(xsi, self._setup_strobe_port, self._setup_data_port)
             expected_cpha = self.get_setup_data(xsi, self._setup_strobe_port, self._setup_data_port)
@@ -48,28 +48,27 @@ class SPIMasterChecker(xmostest.SimThread):
 
             all_ss_deserted = True
             for i in range(len(self._ss_ports)):
-               if (xsi.sample_port_pins(self._ss_ports[i]) == 0):
-		 all_ss_deserted = False
-                 break
+                if (xsi.sample_port_pins(self._ss_ports[i]) == 0):
+                    all_ss_deserted = False
+                    break
 
             while not all_ss_deserted:
-              self.wait_for_port_pins_change(self._ss_ports)
-              all_ss_deserted = True
-              for i in range(len(self._ss_ports)):
-                all_ss_deserted = all_ss_deserted and (xsi.sample_port_pins(self._ss_ports[i]) == 1)
+                self.wait_for_port_pins_change(self._ss_ports)
+                all_ss_deserted = True
+                for i in range(len(self._ss_ports)):
+                    all_ss_deserted = all_ss_deserted and (xsi.sample_port_pins(self._ss_ports[i]) == 1)
 
             error = False
 
             active_slave = -1
 
             while(active_slave == -1):
-              self.wait_for_port_pins_change(self._ss_ports)
+                self.wait_for_port_pins_change(self._ss_ports)
 
-              for i in range(len(self._ss_ports)):
-                if xsi.sample_port_pins(self._ss_ports[i]) == 0:
-                  active_slave = i
-                  break
-
+                for i in range(len(self._ss_ports)):
+                    if xsi.sample_port_pins(self._ss_ports[i]) == 0:
+                        active_slave = i
+                        break
 
             last_clock_event_time = xsi.get_time();
 
@@ -81,79 +80,78 @@ class SPIMasterChecker(xmostest.SimThread):
             tx_byte = tx_data[0]
             # check the polarity
             if xsi.sample_port_pins(self._sck_port) != expected_cpol:
-              print("ERROR: unexpected clock polarity at the slave select point")
-              error = True
+                print("ERROR: unexpected clock polarity at the slave select point")
+                error = True
             clock_edge_number = 0
 
             #probably not physically possible but good for testing
             if expected_cpha == 0:
-              xsi.drive_port_pins(self._miso_port, (tx_byte>>7)&1)
-              tx_bit_counter += 1
-              tx_byte = tx_byte << 1
+                xsi.drive_port_pins(self._miso_port, (tx_byte>>7)&1)
+                tx_bit_counter += 1
+                tx_byte = tx_byte << 1
 
             ss_value = xsi.sample_port_pins(self._ss_ports[active_slave])
             sck_value = xsi.sample_port_pins(self._sck_port)
 
             while ss_value == 0:
-              self.wait_for_port_pins_change(self._ss_ports + [self._sck_port])
+                self.wait_for_port_pins_change(self._ss_ports + [self._sck_port])
 
-              for i in range(len(self._ss_ports)):
-                if i != active_slave and xsi.sample_port_pins(self._ss_ports[i]) == 0:
-                  error = True
-                  print "Second slave selected during first transaction"
-
-              if (ss_value == xsi.sample_port_pins(self._ss_ports[active_slave]) and (sck_value == xsi.sample_port_pins(self._sck_port))):
-                continue
-
-              ss_value = xsi.sample_port_pins(self._ss_ports[active_slave])
-              sck_value = xsi.sample_port_pins(self._sck_port)
-
-              if ss_value == 0:
-                clock_event_time = xsi.get_time();
-                measured_time_elapsed = clock_event_time - last_clock_event_time
-                if clock_edge_number > 1 and (measured_time_elapsed*1.05) < clock_half_period :
-                  print("ERROR: Clock half period less than allowed for given SCLK frequency" )
-                  print("%d %d " % (measured_time_elapsed, clock_half_period))
-                  error = True
-                last_clock_event_time =clock_event_time
-
-              #check that the clock edges never go faster than the expected clock rate
-              if ss_value == 0:
-                clock_edge_number += 1
-               #print clock_edge_number
-                #the the clock must have transitioned
-                if sck_value == (expected_cpha ^ expected_cpol):
-                  if expected_miso_enabled == 1:
-                    #drive data out
-                    xsi.drive_port_pins(self._miso_port, (tx_byte>>7)&1)
-                    tx_bit_counter += 1
-                    tx_byte = tx_byte << 1
-                    if (tx_bit_counter%8) == 0:
-                      index = tx_bit_counter/8
-                      if index < 16:
-                        tx_byte = tx_data[tx_bit_counter/8]
-                      else:
-                        tx_byte = 0;
-
-                else:
-                  #clock data in
-                  if expected_mosi_enabled == 1:
-                    rx_byte = rx_byte << 1
-                    rx_byte += xsi.sample_port_pins(self._mosi_port)
-                    rx_bit_counter = rx_bit_counter + 1
-                    if((rx_bit_counter%8) == 0):
-                      expected_rx_byte = rx_data[(rx_bit_counter/8) - 1]
-                      #print "slave got {seen} and expected {expect}".format(seen=rx_byte, expect=expected_rx_byte)
-                      if expected_rx_byte != rx_byte:
-                        print "ERROR: slave recieved incorrect data Got:%02x Expected:%02x"%(rx_byte, expected_rx_byte)
+                for i in range(len(self._ss_ports)):
+                    if i != active_slave and xsi.sample_port_pins(self._ss_ports[i]) == 0:
                         error = True
-                      rx_byte = 0
-              else:
-                if clock_edge_number != expected_num_bytes*2*8:
-                  error = True
-                  print "ERROR: incorrect number of clock edges at slave {seen}/{expect}".format(seen=clock_edge_number, expect=expected_num_bytes*2*8)
-                if error:
-                  print "Fail: CPOL:{cpol} CPHA:{cpha} KHz:{freq} MOSI Enabled:{mosi_enabled} MISO Enabled:{miso_enabled}".format(
-			cpol=expected_cpol, cpha=expected_cpha,
-                        mosi_enabled = expected_mosi_enabled, miso_enabled = expected_miso_enabled,
-                        freq=expected_frequency_in_khz)
+                        print "Second slave selected during first transaction"
+
+                if (ss_value == xsi.sample_port_pins(self._ss_ports[active_slave]) and (sck_value == xsi.sample_port_pins(self._sck_port))):
+                    continue
+
+                ss_value = xsi.sample_port_pins(self._ss_ports[active_slave])
+                sck_value = xsi.sample_port_pins(self._sck_port)
+
+                if ss_value == 0:
+                    clock_event_time = xsi.get_time();
+                    measured_time_elapsed = clock_event_time - last_clock_event_time
+                    if clock_edge_number > 1 and (measured_time_elapsed*1.05) < clock_half_period :
+                        print("ERROR: Clock half period less than allowed for given SCLK frequency" )
+                        print("%d %d " % (measured_time_elapsed, clock_half_period))
+                        error = True
+                    last_clock_event_time =clock_event_time
+
+                #check that the clock edges never go faster than the expected clock rate
+                if ss_value == 0:
+                    clock_edge_number += 1
+                    #print clock_edge_number
+                    #the the clock must have transitioned
+                    if sck_value == (expected_cpha ^ expected_cpol):
+                        if expected_miso_enabled == 1:
+                            #drive data out
+                            xsi.drive_port_pins(self._miso_port, (tx_byte>>7)&1)
+                            tx_bit_counter += 1
+                            tx_byte = tx_byte << 1
+                            if (tx_bit_counter%8) == 0:
+                                index = tx_bit_counter/8
+                                if index < 16:
+                                    tx_byte = tx_data[tx_bit_counter/8]
+                                else:
+                                    tx_byte = 0;
+                    else:
+                        #clock data in
+                        if expected_mosi_enabled == 1:
+                            rx_byte = rx_byte << 1
+                            rx_byte += xsi.sample_port_pins(self._mosi_port)
+                            rx_bit_counter = rx_bit_counter + 1
+                            if((rx_bit_counter%8) == 0):
+                                expected_rx_byte = rx_data[(rx_bit_counter/8) - 1]
+                                #print "slave got {seen} and expected {expect}".format(seen=rx_byte, expect=expected_rx_byte)
+                                if expected_rx_byte != rx_byte:
+                                    print "ERROR: slave recieved incorrect data Got:%02x Expected:%02x"%(rx_byte, expected_rx_byte)
+                                    error = True
+                                rx_byte = 0
+                else:
+                    if clock_edge_number != expected_num_bytes*2*8:
+                        error = True
+                        print "ERROR: incorrect number of clock edges at slave {seen}/{expect}".format(seen=clock_edge_number, expect=expected_num_bytes*2*8)
+                    if error:
+                        print "Fail: CPOL:{cpol} CPHA:{cpha} KHz:{freq} MOSI Enabled:{mosi_enabled} MISO Enabled:{miso_enabled}".format(
+                            cpol=expected_cpol, cpha=expected_cpha,
+                            mosi_enabled = expected_mosi_enabled, miso_enabled = expected_miso_enabled,
+                            freq=expected_frequency_in_khz)
